bc32a769baae283eb8cf8fcbc41ee3955c7a0e9e
Nikomixo <kevint0218@outlook.com>
2023-05-05 20:16:15 -0700

    block

---
+++ block.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+#include "block.h"
+#include "image.h"
+
+unsigned char *bread(int block_num, unsigned char *block)
+{
+    lseek(image_fd, block_num * BLOCK_SIZE, SEEK_SET);
+    read(image_fd, block, sizeof(block));
+
+    return block;
+}
+
+void bwrite(int block_num, unsigned char *block)
+{
+    lseek(image_fd, block_num * BLOCK_SIZE, SEEK_SET);
+    write(image_fd, block, sizeof(block));
+}
